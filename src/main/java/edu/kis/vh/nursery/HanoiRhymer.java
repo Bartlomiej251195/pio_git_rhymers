36c3ab9d89bf7f984680e9601579b5174ac7a1e5
Fallen <251195.edu.p.lodz.pl>
2025-04-09 19:20:19 +0200

    Stworzenie szkieletu dokumentacji dla pliku HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa odpowiadająca za stos
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Licznik odrzuconych wartości.
+     */
     private int totalRejected = 0;
 
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Dodaje element do stosu gdy jest mniejszy lub równy poprzedniemu
+     * @param in wartosc do dodania
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
